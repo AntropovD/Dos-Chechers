54108249d3cb746e0a17a074d99562fdb8ff58b2
Dantre <dantreKrt@gmail.com>
2015-12-04 18:15:35 +0500

    могу не могу делать ход мессдж

--- LIB/GAME.asm
+++ LIB/GAME.asm
@@ -1,5 +1,36 @@
+
+;===============================================================
 ; cx - from, dx - to
-Can_Make_Move proc
-	mov ax, 0
+
+
+;===============================================================
+AddMessage_Cannot_Make_Move proc
+	mov di, offset BufferString
+	mov si, offset cannot_make_move_msg
+	mov cx, 10
+	repne movsb
+
+	mov al, ' '
+	mov cx, 17
+	repne stosb
+
+	call Add_BufferString_To_History
 	ret
-Can_Make_Move endp
+	cannot_make_move_msg db 'wrong move'	
+AddMessage_Cannot_Make_Move endp
+;===============================================================
+AddMessage_Can_Make_Move proc
+	mov di, offset BufferString
+	mov si, offset can_make_move_msg
+	mov cx, 10
+	repne movsb
+
+	mov al, ' '
+	mov cx, 17
+	repne stosb
+
+	call Add_BufferString_To_History
+	ret
+	can_make_move_msg db 'goood move'	
+AddMessage_Can_Make_Move endp
+;===============================================================
