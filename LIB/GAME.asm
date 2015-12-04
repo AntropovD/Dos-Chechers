e9bd412018d0ae46e430441a66cdbd265dfb17b2
Dantre <dantreKrt@gmail.com>
2015-12-04 19:57:07 +0500

    Делаешь шаг, шашка исчезает

--- LIB/GAME.asm
+++ LIB/GAME.asm
@@ -34,3 +34,45 @@ AddMessage_Can_Make_Move proc
 	can_make_move_msg db 'goood move'	
 AddMessage_Can_Make_Move endp
 ;===============================================================
+Can_Make_Move proc
+	push cx dx
+
+	mov ax, cx
+	;твой цвет?
+	call Get_Board_Value_By_AX_to_AL
+	cmp al, Your_Colour
+	jne fail_make_move
+	; пустая клетка
+	mov ax, dx
+	call Get_Board_Value_By_AX_to_AL
+	cmp al, 0
+	jne fail_make_move
+
+	sub dx, cx
+	cmp dh, 1
+	jg fail_make_move
+	cmp dl, 1
+	jg fail_make_move
+
+		
+		
+		call AddMessage_Can_Make_Move
+		mov ax, 1
+		pop dx cx
+		ret
+		
+	fail_make_move:
+		call AddMessage_Cannot_Make_Move		
+		mov ax, 0
+		pop dx cx
+		ret
+Can_Make_Move endp
+;===============================================================
+;===============================================================
+;===============================================================
+;===============================================================
+
+
+
+
+
